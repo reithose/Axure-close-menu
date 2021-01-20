204ec22c2dfa686244b8921d31423e774d7cbe36
Joris <jorisreijbroek@gmail.com>
2021-01-20 20:02:48 +0100

    New script
    Works on Axure 9

--- closemenu.js
+++ closemenu.js
@@ -1,17 +1,3 @@
-$( document ).ready(function() {
- parent.$("ul").find("ul").css({"display":"none"});
- parent.$("body").find('.sitemapMinus').removeClass('sitemapMinus').addClass('sitemapPlus')
- parent.$("body").find('.sitemapPlusMinusLink').unbind();
- parent.$("body").find('.sitemapPlusMinusLink').toggle(
-  function(){
-   $(this)
-        .children('.sitemapPlus').removeClass('sitemapPlus').addClass('sitemapMinus').end()
-         .closest('li').children('ul').css({"display":"block"});
- },
-  function(){
-   $(this)
-         .children('.sitemapMinus').removeClass('sitemapMinus').addClass('sitemapPlus').end()
-         .closest('li').children('ul').css({"display":"none"});
-}
-);
-});
+<script type="text/javascript">
+    javascript:void(window.parent.jQuery.fn.reverse = [].reverse); void(window.parent.$('.sitemapMinus').reverse().each(function(){this.click()}));
+</script>
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-Script to collapse the page tree in the navigation menu in Axure HTML exports on page load. Helpful for large Axure exports.
-Add this script as a plugin on [share.axure.com](https://share.axure.com) or in index.html in your local export.
+Script to collapse the page tree in the navigation menu in Axure HTML exports on page load. Helpful for large Axure exports. Tested in Axure 8 and 9.
+
+
+Add this script as a plugin on [app.axure.cloud](https://app.axure.cloud) or in index.html in your local export. Add only to the frontpage to close the menu on load. Otherwise the menu closes while navigating through your model which is annoying. 
+
+Source: [forum.axure.com](https://forum.axure.com/t/generate-prototype-sitemap-folders-collapsed-vs-expanded)
